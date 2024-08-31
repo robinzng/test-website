89c75211e1eb17a2848c3e27f8c8915d63a88de9
robinzng <robinzeng1@gmail.com>
2024-08-31 14:36:01 -0400

    Update day_night.js

--- scripts/day_night.js
+++ scripts/day_night.js
@@ -1,4 +1,9 @@
-function changepic()
-{
- document.getElementById("sun").style.top=2000 + "px";  
-}
+const sunImage = document.querySelector("sun");
+
+sunImage.onclick = () => {
+  const sunTopPosition = window.getComputedStyle(sunImage).getPropertyValue("top");
+  const sunRightPosition = window.getComputedStyle(sunImage).getPropertyValue("right");
+
+  console.log(sunTopPosition);
+  console.log(sunRightPosition);  
+};
