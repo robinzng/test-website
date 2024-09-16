d8f286a5dcdb3527b20dbaf239d81277a5837a05
robinzng <robinzeng1@gmail.com>
2024-09-16 13:12:16 -0400

    move sun!

--- scripts/day_night.js
+++ scripts/day_night.js
@@ -4,6 +4,18 @@ sunImage.onclick = () => {
   const sunTopPosition = window.getComputedStyle(sunImage).getPropertyValue("top");
   const sunBottomPosition = window.getComputedStyle(sunImage).getPropertyValue("bottom");
 
+  if (sunTopPosition < sunBottomPosition) {
+    sunImage.style.top='auto';
+    sunImage.style.right='auto';
+    sunImage.style.bottom='2vh';
+    sunImage.style.left='2vh';
+  } else {
+    sunImage.style.top='2vh';
+    sunImage.style.right='2vh';
+    sunImage.style.bottom='auto';
+    sunImage.style.left='auto';
+  }
+
   console.log(sunTopPosition);
   console.log(sunBottomPosition);  
 };
